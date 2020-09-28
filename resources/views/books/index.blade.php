b8bed3712b18c1e2aa0b0bfebe8abea0106b94ee
bhalawaty <bilal.halawaty@gmail.com>
2020-09-28 19:08:03 +0200

    test

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,7 +3,7 @@
 
     <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
         <div class="col-md-6 px-0">
-            <h1 class="display-4 font-italic">confingure jenkins with ec2 finallllly Title of a longer featured blog
+            <h1 class="display-4 font-italic">configure jenkins with ec2 finallllly Title of a longer featured blog
                 post </h1>
             <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and
                 efficiently about what’s most interesting in this post’s contents.</p>
