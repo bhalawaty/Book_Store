dfecdcac7715a772c2a7664785c4fe726ec27f5d
bilal elhalawaty <bilal.halawaty@gmail.com>
2021-01-23 12:35:47 +0200

    2

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,7 +3,7 @@
 
     <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
         <div class="col-md-6 px-0">
-            <h1 class="display-4 font-italic"> 1 add GitHub hook trigger for GITScm polling Title of a longer featured
+            <h1 class="display-4 font-italic"> 2 add GitHub hook trigger for GITScm polling Title of a longer featured
                 blog
                 post </h1>
             <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and
